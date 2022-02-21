83e216e90e6541d8299415b36463b60083c78770
Márcio Gadelha <marciogadelha89@gmail.com>
2022-02-21 11:55:54 -0300

    Request bypassed to backend.

--- app.js
+++ app.js
@@ -1,59 +1,54 @@
-class CredibankClientConfig {
-    constructor(host, port) {
-        this.host = host;
-        this.port = port;
-    }
-}
-
-var config;
-
-fetch("appsettings.json")
-    .then(results => results.json()
-        .then(data => {
-            const host = data.CredibankClientConfig.Host
-            const port = data.CredibankClientConfig.Port
-            config = new CredibankClientConfig(host, port)
-        })
-    )
 
 async function getDigitalCheck() {
     const accountID = document.getElementById("accountID").value
     const amount = document.getElementById("amount").value
-    try{
-        const response = await axios.get(`${config.host}:${config.port}/check/${accountID}/amount/${amount}`)
-        console.log(response.data)
-        const datetime = new Date(response.data.date)
-        const date = datetime.toLocaleDateString()
-        const time = datetime.toTimeString()
-        $("#resultStatus").text("Successful transaction!")
-        $("#resultAccount").text("Account ID: " + accountID)
-        $("#resultAmount").text("Amount: " + amount + "€")
-        $("#resultDate").text("Date: " + date)
-        $("#resultTime").text("Time: " + time)
-        $("#resultCheckID").text("Check ID: " + response.data.checkID)
-        $("#resultStatus").show()
-        $("#resultAccount").show()
-        $("#resultAmount").show()
-        $("#resultDate").show()
-        $("#resultTime").show()
-        $("#resultCheckID").show()
+    try {
+        const response = await fetch("/getdigitalcheck", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ accountID: accountID, amount: amount })
+        });
+        response.text().then(function (text) {
+            const data = JSON.parse(text);
+            console.log(data)
+            if (data.status == "OK") {
+                const datetime = new Date(data.date)
+                const date = datetime.toLocaleDateString()
+                const time = datetime.toTimeString()
+                $("#resultStatus").text(data.message)
+                $("#resultAccount").text("Account ID: " + accountID)
+                $("#resultAmount").text("Amount: " + amount + "€")
+                $("#resultDate").text("Date: " + date)
+                $("#resultTime").text("Time: " + time)
+                $("#resultCheckID").text("Check ID: " + data.checkID)
+                $("#resultStatus").show()
+                $("#resultAccount").show()
+                $("#resultAmount").show()
+                $("#resultDate").show()
+                $("#resultTime").show()
+                $("#resultCheckID").show()
+            } else {
+                $("#resultStatus").text(data.message)
+                $("#resultStatus").show()
+            }
+        });
     } catch (error) {
-        if (accountID != "" && amount <= 0) {
-            $("#resultStatus").text("Transaction failed! No amount specified.")
-        }
-        else {
-            $("#resultStatus").text("Transaction failed! Verify account ID number.")
-        }
+        $("#resultStatus").text("Transaction failed!")
         $("#resultStatus").show()
         console.log(error)
     }
 }
 
 function validCurrency(id, value) {
-    document.getElementById(id).value = parseFloat(value).toFixed(2);
+    if (value > 0) {
+        document.getElementById(id).value = parseFloat(value).toFixed(2);
+    }
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     document.getElementById('resultModal').addEventListener('hidden.bs.modal', function (event) {
         $("#resultStatus").hide()
         $("#resultAccount").hide()
--- server.js
+++ server.js
@@ -1,3 +1,42 @@
+const axios = require('axios').default;
+
+class CredibankClientConfig {
+  constructor(host, port) {
+    this.host = host;
+    this.port = port;
+  }
+}
+
+const appsettings = require("./appsettings.json")
+const config = new CredibankClientConfig(appsettings.CredibankClientConfig.Host, appsettings.CredibankClientConfig.Port)
+
+async function getDigitalCheck(request, response) {
+  console.log(request.body)
+  const accountID = request.body.accountID
+  const amount = request.body.amount
+  try {
+    var result = await axios.get(`${config.host}:${config.port}/check/${accountID}/amount/${amount}`)
+    result.data["status"] = "OK"
+    result.data["message"] = "Successful transaction!"
+    console.log(result.data)
+    response.send(result.data)
+  } catch (error) {
+    var resultError = {}
+    resultError["status"] = "Failed"
+    if (accountID.length != 8) {
+      resultError["message"] = "Transaction failed! Verify account ID number."
+    }
+    else if (amount == '' || amount <= 0) {
+      resultError["message"] = "Transaction failed! No amount specified."
+    }
+    else {
+      resultError["message"] = "Transaction failed! " + error.message
+    }
+    console.log(resultError)
+    response.send(resultError)
+  }
+}
+
 const express = require('express')
 const cors = require('cors')
 
@@ -12,6 +51,13 @@ app.use(express.static('./'));
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 // }
 app.use(cors())
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.post('/getdigitalcheck', function (req, res) {
+  getDigitalCheck(req, res)
+})
 
 app.listen(port, () => {
   console.log(`Server running at port ${port}`)
